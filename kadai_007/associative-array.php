d0efbdf3216b40523c38d25b836eeeca6ce4a179
takashi-saito1226 <139130594+takashi-saito1226@users.noreply.github.com>
2023-08-01 20:43:36 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+<body>
+    <p>
+        <?php
+        $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($vegetable_data);
+
+        ?>
+    </p>
+</body>
+
+</html>
